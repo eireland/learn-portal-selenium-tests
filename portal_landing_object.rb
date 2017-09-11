1a99c05e26ebdca185e4d2971af89f970999f2a4
Evangeline Ireland <eireland@concord.org>
2017-09-11 15:21:33 -0700

    Updated selectors for landing page for website redesign

--- portal_landing_object.rb
+++ portal_landing_object.rb
@@ -1,15 +1,28 @@
 module PortalLandingObject
-  LOGIN_BUTTON = {css: '#accounts > a:nth-child(2)'}
-  LOGIN_MODAL = {id: 'log-in'}
-  SIGN_UP_BUTTON = {css: '#accounts > a:nth-child(1)'}
-  SIGNUP_MODAL = {id: 'sign-up'}
-  LOGIN_MODAL_CLOSE = {css: '#log-in > a'}
-  SIGNUP_MODAL_CLOSE = {css:'#sign-up > a'}
+  LOGIN_BUTTON = {css: '.portal-pages-main-nav-item > .log-in'}
+  REGISTER_BUTTON = {css: '.portal-pages-main-nav-item > .register'}
+  COLLECTION_LINK = {css: '.portal-pages-main-nav-collections'}
+  ABOUT_LINK = {css: '.portal-pages-main-nav-about'}
+  SRF_HOME_LINK = {css: 'concord-logo'}
+
+  LOGIN_MODAL = {id: 'login-default-modal'}
+  LOGIN_SUBMIT_BUTTON = {css: '.login-default-modal-content > .signup-form > .submit_button-container > .submit-btn'}
+  LOGIN_MODAL_GOOGLE_OPTION = {id: 'google'}
+  LOGIN_MODAL_SCHOOLOGY_OPTION = {id: 'schoology'}
+  LOGIN_MODAL_FORGOT_PASSWORD = {xpath: "#login-default-modal > div > form > div.submit-button-container > a"}
+
+  MODAL_CLOSE = {css: '.portal-page-close'}
+
+  SIGNUP_MODAL = {id: 'signup-default-modal'}
+  SIGNUP_SUBMIT_BUTTON = {css: '.signup-default-modal-content > .signup-form > .submit_button-container > .submit-btn'}
+  LOGIN_MODAL_GOOGLE_OPTION = {id: 'google'}
+  LOGIN_MODAL_SCHOOLOGY_OPTION = {id: 'schoology'}
+
   LIFE_SCIENCE_FILTER = {id: 'life-sciences'}
   ENGINEERING_TECHNOLOGY_FILTER = {id: 'engineering-technology'}
   EARTH_SPACE_SCIENCES_FILTER = {id: 'earth-space-sciences'}
   PHYSICAL_SCIENCES_FILTER = {id: 'physical-sciences'}
-  SEARCH_KEYWORD = {id: 'keyword'}
+  SEARCH_FIELD = {xpath: '//div[contains(@class,"portal-pages-search-input-container")]/input'}
   ADVANCE_SEARCH = {id: 'advance-search-link'}
   SEARCH_BUTTON = {css: '#search > form > input.button'}
   ELEMENTARY_SCHOOL_FILTER = {id: 'elementary-school'}
@@ -33,6 +46,16 @@ module PortalLandingObject
     # verify_element(element)
   end
 end
+
+  def click_link(link)
+    case (link)
+      when "collection"
+        element = COLLECTION_LINK
+    end
+
+    wait_for {displayed? (element)}
+    # verify_page (element)
+  end
 
 def signup
   puts 'In sign up dialog'
