61214589c726f2eb08ef734e84e83c9b1863fbca
Evangeline Ireland <eireland@concord.org>
2017-09-11 15:20:37 -0700

    Added Collections Page object

---
+++ collection_page_object.rb
@@ -0,0 +1,8 @@
+module CollectionPageObject
+  COLLECTION_HEADER = {css: '.portal-pages-collections-page-header'}
+  COLLECTION_CARD = {css: '.portal-pages-collections-card'}
+  COLLECTION_CARD_IMAGE = {css: '.portal-pages-collections-card-image-preview'}
+  COLLECTION_CARD_NAME = {css: '.portal-pages-collections-card-name'}
+  COLLECTION_CARD_DESCRIPTION = {css: '.portal-pages-collections-card-description'}
+  SRF_HOME_LINK = {css: '.special-link'}
+end
